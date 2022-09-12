5cd3958f9df8c23e7e90a5f641b7c7ab65093503
Nicolas JEANNE <jeanne.n@chu-toulouse.fr>
2022-09-12 12:57:24 +0200

    start coding

---
+++ contacts_trajectories_info.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+"""
+Created on 12 Sep. 2022
+"""
+
+__author__ = "Nicolas JEANNE"
+__copyright__ = "GNU General Public License"
+__email__ = "jeanne.n@chu-toulouse.fr"
+__version__ = "1.0.0"
+
+import argparse
+import logging
+import os
+import sys
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import seaborn as sns
+
+
+def create_log(path, level):
+    """Create the log as a text file and as a stream.
+
+    :param path: the path of the log.
+    :type path: str
+    :param level: the level og the log.
+    :type level: str
+    :return: the logging:
+    :rtype: logging
+    """
+
+    log_level_dict = {"DEBUG": logging.DEBUG,
+                      "INFO": logging.INFO,
+                      "WARNING": logging.WARNING,
+                      "ERROR": logging.ERROR,
+                      "CRITICAL": logging.CRITICAL}
+
+    if level is None:
+        log_level = log_level_dict["INFO"]
+    else:
+        log_level = log_level_dict[args.log_level]
+
+    if os.path.exists(path):
+        os.remove(path)
+
+    logging.basicConfig(format="%(asctime)s %(levelname)s:\t%(message)s",
+                        datefmt="%Y/%m/%d %H:%M:%S",
+                        level=log_level,
+                        handlers=[logging.FileHandler(path), logging.StreamHandler()])
+    return logging
+
+
+if __name__ == "__main__":
+    descr = f"""
+    {os.path.basename(__file__)} v. {__version__}
+
+    Created by {__author__}.
+    Contact: {__email__}
+    {__copyright__}
+
+    Distributed on an "AS IS" basis without warranties or conditions of any kind, either express or implied.
+
+    From a CSV of the amino acids contacts file produced by the script trajectories
+    (https://github.com/njeanne/trajectories), extract the information of the contacts outside the neighbourhood 
+    contacts of an amino acid and determine to which domain the acceptor amino acid belongs to using a BED file 
+    registering the domains.
+    """
+    parser = argparse.ArgumentParser(description=descr, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-o", "--out", required=True, type=str, help="the path to the output directory.")
+    parser.add_argument("-b", "--bed", required=True, type=str,
+                        help="the path to the BED file registrering the amino acids domains positions.")
+    parser.add_argument("-f", "--format", required=False, default="svg",
+                        choices=["eps", "jpg", "jpeg", "pdf", "pgf", "png", "ps", "raw", "svg", "svgz", "tif", "tiff"],
+                        help="the output plots format: 'eps': 'Encapsulated Postscript', "
+                             "'jpg': 'Joint Photographic Experts Group', 'jpeg': 'Joint Photographic Experts Group', "
+                             "'pdf': 'Portable Document Format', 'pgf': 'PGF code for LaTeX', "
+                             "'png': 'Portable Network Graphics', 'ps': 'Postscript', 'raw': 'Raw RGBA bitmap', "
+                             "'rgba': 'Raw RGBA bitmap', 'svg': 'Scalable Vector Graphics', "
+                             "'svgz': 'Scalable Vector Graphics', 'tif': 'Tagged Image File Format', "
+                             "'tiff': 'Tagged Image File Format'. Default is 'svg'.")
+    parser.add_argument("-d", "--distance-contacts", required=False, type=float, default=3.0,
+                        help="the contacts distances threshold, default is 3.0 Angstroms.")
+    parser.add_argument("-l", "--log", required=False, type=str,
+                        help="the path for the log file. If this option is skipped, the log file is created in the "
+                             "output directory.")
+    parser.add_argument("--log-level", required=False, type=str,
+                        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+                        help="set the log level. If the option is skipped, log level is INFO.")
+    parser.add_argument("--version", action="version", version=__version__)
+    parser.add_argument("input", type=str, help="the donors/acceptors contacts CSV file.")
+    args = parser.parse_args()
+
+    # create output directory if necessary
+    os.makedirs(args.out, exist_ok=True)
+
+    # create the logger
+    if args.log:
+        log_path = args.log
+    else:
+        log_path = os.path.join(args.out, f"{os.path.splitext(os.path.basename(__file__))[0]}.log")
+    create_log(log_path, args.log_level)
+
+    logging.info(f"version: {__version__}")
+    logging.info(f"CMD: {' '.join(sys.argv)}")
+
