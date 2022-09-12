3d5e8079664fdb13afadff96cf20b2938edddfb0
Nicolas JEANNE <jeanne.n@chu-toulouse.fr>
2022-09-12 18:28:41 +0200

    added extract roi function

--- trajectories_contacts_regions.py
+++ trajectories_contacts_regions.py
@@ -12,6 +12,7 @@ __version__ = "1.0.0"
 import argparse
 import logging
 import os
+import re
 import sys
 
 import pandas as pd
@@ -52,6 +53,25 @@ def create_log(path, level):
     return logging
 
 
+def extract_roi(roi_to_extract):
+    """Extract the start and stop coordinates of the region of interest (roi).
+
+    :param roi_to_extract: the coordinates ot the region of interest, as 100-200 i.e.
+    :type roi_to_extract: str
+    :raises ArgumentTypeError: is not between 0.0 and 100.0
+    :return: the start and stop coordinates of the region of interest.
+    :rtype: list
+    """
+    pattern_roi_to_extract = re.compile("(\\d+)-(\\d+)")
+    match_roi_to_extract = pattern_roi_to_extract.search(roi_to_extract)
+    if match_roi_to_extract:
+        roi_extracted = [int(match_roi_to_extract.group(1)), int(match_roi_to_extract.group(2))]
+    else:
+        raise argparse.ArgumentTypeError(f"'{roi_to_extract}' argument is malformed, it should be two integers "
+                                         f"separated by an hyphen, i.e: '100-200'.")
+    return roi_extracted
+
+
 if __name__ == "__main__":
     descr = f"""
     {os.path.basename(__file__)} v. {__version__}
@@ -64,13 +84,14 @@ if __name__ == "__main__":
 
     From a CSV of the amino acids contacts file produced by the script trajectories
     (https://github.com/njeanne/trajectories), extract the information of the contacts outside the neighbourhood 
-    contacts of an amino acid and determine to which domain the acceptor amino acid belongs to using a BED file 
+    contacts of an amino acid and determine to which region the acceptor amino acid belongs to using a BED file 
     registering the domains.
     """
     parser = argparse.ArgumentParser(description=descr, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("-o", "--out", required=True, type=str, help="the path to the output directory.")
     parser.add_argument("-b", "--bed", required=True, type=str,
-                        help="the path to the BED file registrering the amino acids domains positions.")
+                        help="the path to the BED file registering the amino acids regions positions.")
+    parser.add_argument("-i", "--roi", required=False, type=str)
     parser.add_argument("-f", "--format", required=False, default="svg",
                         choices=["eps", "jpg", "jpeg", "pdf", "pgf", "png", "ps", "raw", "svg", "svgz", "tif", "tiff"],
                         help="the output plots format: 'eps': 'Encapsulated Postscript', "
@@ -80,8 +101,11 @@ if __name__ == "__main__":
                              "'rgba': 'Raw RGBA bitmap', 'svg': 'Scalable Vector Graphics', "
                              "'svgz': 'Scalable Vector Graphics', 'tif': 'Tagged Image File Format', "
                              "'tiff': 'Tagged Image File Format'. Default is 'svg'.")
-    parser.add_argument("-d", "--distance-contacts", required=False, type=float, default=3.0,
-                        help="the contacts distances threshold, default is 3.0 Angstroms.")
+    parser.add_argument("-a", "--atoms-distance", required=False, type=float, default=3.0,
+                        help="the atoms contacts distances threshold between two residues, default is 3.0 Angstroms.")
+    parser.add_argument("-r", "--residues-distance", required=False, type=int, default=10,
+                        help="when 2 atoms of different residues are in contact, the minimal distance in number of "
+                             "residues that should separate them for a long range interaction, default is 10.")
     parser.add_argument("-l", "--log", required=False, type=str,
                         help="the path for the log file. If this option is skipped, the log file is created in the "
                              "output directory.")
@@ -105,3 +129,17 @@ if __name__ == "__main__":
     logging.info(f"version: {__version__}")
     logging.info(f"CMD: {' '.join(sys.argv)}")
 
+    # load the BED file
+    bed = pd.read_csv(args.bed, sep="\t", header=None, names=["id", "start", "stop", "region"])
+    print(bed)
+
+    # check the region of interest if any
+    if args.roi:
+        try:
+            roi = extract_roi(args.roi, bed)
+        except argparse.ArgumentTypeError as exc:
+            logging.info(exc, exc_info=True)
+            sys.exit(1)
+
+    # load the contacts file
+    # contacts
