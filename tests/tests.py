380b964d3583e9401e30a498e3e534d0ff7fe8b8
Nicolas JEANNE <jeanne.n@chu-toulouse.fr>
2022-09-15 09:44:09 +0200

    updated tests files

--- tests/tests.py
+++ tests/tests.py
@@ -16,7 +16,8 @@ import uuid
 
 import pandas as pd
 from pandas.testing import assert_frame_equal
-from trajectories_outliers_contacts import extract_roi, extract_contacts_with_atoms_distance
+from trajectories_outliers_contacts import extract_bed, extract_roi, extract_contacts_with_atoms_distance, \
+    outliers_contacts, update_regions
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 TEST_FILES_DIR = os.path.join(TEST_DIR, "test_files")
@@ -27,35 +28,44 @@ sys.path.append(BIN_DIR)
 class TestTrajectoriesOutliersContacts(unittest.TestCase):
 
     def setUp(self):
-        system_tmp_dir = tempfile.gettempdir()
-        self.tmp_dir = os.path.join(system_tmp_dir, "tmp_tests_trajectories")
+        self.tmp_dir = os.path.join(tempfile.gettempdir(), "tmp_tests_trajectories")
         os.makedirs(self.tmp_dir, exist_ok=True)
         self.format_output = "svg"
         self.atoms_dist = 3.0
         self.residues_dist = 10
         self.roi = "682-838"
         self.col_distance = "whole_MD_median_distance"
+        self.path_bed = os.path.join(TEST_FILES_DIR, "HEPAC-26_RPL6_ORF1.bed")
+        self.input_contacts = os.path.join(TEST_FILES_DIR, "contacts_HEPAC-26_RPL6_ORF1_0_MD_0_to_30M.csv")
+        self.extracted_bed = pd.read_csv(os.path.join(TEST_FILES_DIR, "extracted_bed_HEPAC-26_RPL6_ORF1.csv"),
+                                         sep=",", index_col=0)
         self.expected_filtered_contacts = pd.read_csv(os.path.join(TEST_FILES_DIR,
                                                                    "filtered_contacts_HEPAC-26_RPL6_ORF1_0.csv"),
                                                       sep=",", index_col=0)
         self.expected_outliers_contacts = pd.read_csv(os.path.join(TEST_FILES_DIR,
                                                                    "outliers_contacts_HEPAC-26_RPL6_ORF1_0.csv"),
                                                       sep=",", index_col=0)
+        self.expected_regions_contacts = pd.read_csv(os.path.join(TEST_FILES_DIR,
+                                                                  "outliers_regions_contacts_HEPAC-26_RPL6_ORF1_0.csv"),
+                                                     sep=",", index_col=0)
 
     def tearDown(self):
         # Clean temporary files
         shutil.rmtree(self.tmp_dir)
 
+    def test_extract_bed(self):
+        assert_frame_equal(self.extracted_bed, extract_bed(self.path_bed))
+
     def test_extract_roi(self):
         self.assertRaises(argparse.ArgumentTypeError, extract_roi, "682_838")
         self.assertRaises(argparse.ArgumentTypeError, extract_roi, "422")
         self.assertRaises(argparse.ArgumentTypeError, extract_roi, "toto")
-        roi_observed = extract_roi(self.roi)
-        self.assertListEqual(roi_observed, [682, 838])
+        observed = extract_roi(self.roi)
+        self.assertListEqual([682, 838], observed)
 
     def test_extract_contacts_with_atoms_distance(self):
-        raw_contacts = os.path.join(TEST_FILES_DIR, "contacts_HEPAC-26_RPL6_ORF1_0_MD_0_to_30M.csv")
-        observed = extract_contacts_with_atoms_distance(raw_contacts, self.roi, self.col_distance, self.atoms_dist)
+        observed = extract_contacts_with_atoms_distance(self.input_contacts, self.roi, self.col_distance,
+                                                        self.atoms_dist)
         assert_frame_equal(self.expected_filtered_contacts, observed)
         # with self.assertRaises(argparse.ArgumentTypeError):
         #     extract_contacts_with_atoms_distance(raw_contacts, "422", self.col_distance, self.atoms_dist)
@@ -67,6 +77,10 @@ class TestTrajectoriesOutliersContacts(unittest.TestCase):
     def test_outliers_contacts(self):
         observed = outliers_contacts(self.expected_filtered_contacts, self.residues_dist, self.col_distance)
         assert_frame_equal(self.expected_outliers_contacts, observed)
+
+    def test_update_regions(self):
+        observed = update_regions(self.expected_outliers_contacts, self.extracted_bed)
+        assert_frame_equal(self.expected_regions_contacts, observed)
 
 
 if __name__ == '__main__':
